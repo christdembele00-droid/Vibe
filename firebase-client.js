63766d725ed7dbcc22713306dadaad12ec9a643d
christdembele00-droid <christdembele00@gmail.com>
2026-09-06 04:48:43 +0000

    Fix Firebase client initialization and export RTDB state

--- firebase-client.js
+++ firebase-client.js
@@ -3,9 +3,11 @@ import { getAuth, onAuthStateChanged, signInWithPopup, signInWithRedirect, getRe
 import { getDatabase } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js';
 import { firebaseConfig } from './firebase-config.js';
 
+const FIREBASE_ENABLED = Boolean(firebaseConfig?.apiKey && firebaseConfig?.projectId && firebaseConfig?.databaseURL);
 const app = getApps()[0] ?? initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getDatabase(app, firebaseConfig.databaseURL);
+const rtdb = db;
 const googleProvider = new GoogleAuthProvider();
 const githubProvider = new GithubAuthProvider();
 
@@ -16,9 +18,11 @@ async function signInAnonymously() {
 }
 
 export {
+  FIREBASE_ENABLED,
   app,
   auth,
   db,
+  rtdb,
   onAuthStateChanged,
   signInAnonymously,
   signInWithPopup,
