0b5080033009698e5c59df3a2312e9067e91057c
christdembele00-droid <christdembele00@gmail.com>
2026-09-06 23:54:51 +0000

    Add Firebase configuration template

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Remplace uniquement les valeurs par la configuration de ton application Firebase Web.
+// Ne mets jamais ici un mot de passe, une clé privée ou un secret serveur.
+export const firebaseConfig = {
+  apiKey: "VOTRE_API_KEY",
+  authDomain: "VOTRE_PROJET.firebaseapp.com",
+  projectId: "VOTRE_PROJET",
+  storageBucket: "VOTRE_PROJET.firebasestorage.app",
+  messagingSenderId: "VOTRE_SENDER_ID",
+  appId: "VOTRE_APP_ID"
+};
