1cbccc4ee620f97bd6e5e65fd51f3adf26ac9f45
christdembele00-droid <christdembele00@gmail.com>
2026-09-07 01:01:48 +0000

    chore: remove Firebase Storage client configuration

--- firebase-config.js
+++ firebase-config.js
@@ -5,7 +5,6 @@ export const firebaseConfig = {
   apiKey: 'AIzaSyC6g40Uc9hq9Ij5DU1nwbO-zwpHqk9L9aQ',
   authDomain: 'vibe-749e5.firebaseapp.com',
   projectId: 'vibe-749e5',
-  storageBucket: 'vibe-749e5.firebasestorage.app',
   messagingSenderId: '17097166235',
   appId: '1:17097166235:web:c39c5c082b3cf6a01ee53e',
   databaseURL: 'https://vibe-749e5-default-rtdb.firebaseio.com'
